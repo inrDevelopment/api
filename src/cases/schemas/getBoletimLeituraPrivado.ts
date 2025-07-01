ecfa49b53502f063443f6bbff0088d31b313406a
Douglas Pacor <douglas@epicquestti.com.br>
2025-07-01 13:58:37 -0300

    doing

--- src/cases/schemas/getBoletimLeituraPrivado.ts
+++ src/cases/schemas/getBoletimLeituraPrivado.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 export const getBoletimLeituraPrivadoValidation = z.object({
-  id: z.number(),
-  idusuario: z.number()
+  id: z.number({ message: "id required" }),
+  idusuario: z.number({ message: "idusuario required" })
 })
 
 export type getBoletimLeituraPrivadoControllerProps = z.input<
